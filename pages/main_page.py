7fcb6c09fee1726ff13a085b47fcd27bf4487f88
Alexander Knyazev <26230870+a13xk@users.noreply.github.com>
2020-08-24 18:48:59 +0300

    Add tests for main page

--- pages/main_page.py
+++ pages/main_page.py
@@ -14,6 +14,23 @@ class MainPage(BasePage):
     DIV_CLASS_SLIDESHOW = (By.CLASS_NAME, "swiper-container.swiper-container-horizontal")
     A_YOUR_STORE = (By.LINK_TEXT, "Your Store")
 
+    BUTTON_CURRENCY = (By.XPATH, "//form[@id='form-currency']/div/button")
+    BUTTON_EUR = (By.XPATH, "//form[@id='form-currency']/div/ul/li/button[@name='EUR']")
+    BUTTON_GBP = (By.XPATH, "//form[@id='form-currency']/div/ul/li/button[@name='GBP']")
+    BUTTON_USD = (By.XPATH, "//form[@id='form-currency']/div/ul/li/button[@name='USD']")
+    STRONG_CURRENT_CURRENCY = (By.XPATH, "//form[@id='form-currency']//strong")
+
+    A_DROPDOWN = (By.XPATH, "//div[@id='top-links']/ul/li[@class='dropdown']/a")
+    A_DROPDOWN_OPEN = (By.XPATH, "//div[@id='top-links']/ul/li[@class='dropdown open']/a")
+    A_REGISTER = (By.XPATH, "(//div[@id='top-links']/ul/li[@class='dropdown open']/ul/li/a)[1]")
+    A_LOGIN = (By.XPATH, "(//div[@id='top-links']/ul/li[@class='dropdown open']/ul/li/a)[2]")
+
+    BUTTON_ADD_TO_CART = (By.XPATH, "(//div[@id='content']//div[@class='row']/div//div[@class='button-group'])[1]/button[1]")
+    A_PRODUCT_NAME = (By.XPATH, "(//div[@id='content']//div[@class='row']/div//div[@class='caption'])[1]/h4/a")
+    BUTTON_VIEW_CART = (By.XPATH, "//div[@id='cart']/button")
+    P_CART_IS_EMPTY = (By.XPATH, "//div[@id='cart']/ul/li/p")
+    A_PRODUCT_ADDED = (By.XPATH, "(//div[@class='alert alert-success alert-dismissible']/a)[1]")
+
     def __init__(self, driver: webdriver, logging_enabled: bool, url: str = ""):
         self.driver: webdriver = driver
         self.url: str = url if url else "https://demo.opencart.com"
@@ -22,5 +39,47 @@ class MainPage(BasePage):
             url=self.url,
             logging_enabled=logging_enabled
         )
+        self.product_name: str = ""
+    #
+
+    def get_current_currency(self) -> str:
+        """ Get currently selected currency for prices """
+        return self.find_element(locator=self.STRONG_CURRENT_CURRENCY).text
+    #
+
+    def change_currency(self, new_currency: str):
+        """ Change currency in top-left corner of main page """
+        self.find_element(locator=self.BUTTON_CURRENCY).click()
+        if new_currency == "EUR":
+            self.find_element(locator=self.BUTTON_EUR).click()
+        if new_currency == "GBP":
+            self.find_element(locator=self.BUTTON_GBP).click()
+        if new_currency == "USD":
+            self.find_element(locator=self.BUTTON_USD).click()
+    #
+
+    def open_my_account_dropdown(self):
+        """ Open My Account dropdown list to check Register and Login links """
+        self.find_element(locator=self.A_DROPDOWN).click()
+    #
+
+    def is_cart_empty(self) -> bool:
+        """ Check if cart is empty at the moment"""
+        self.find_element(locator=self.BUTTON_VIEW_CART).click()
+        if self.find_element(locator=self.P_CART_IS_EMPTY):
+            return True
+        return False
+    #
+
+    def add_to_cart(self):
+        """ Add item from main page to cart"""
+        self.find_element(locator=self.BUTTON_ADD_TO_CART).click()
+        self.product_name = self.find_element(locator=self.A_PRODUCT_NAME).text
+    #
+
+    def check_product_added_to_cart(self) -> bool:
+        """ Check that after clicking "Add to cart" button product was added to cart"""
+        assert self.find_element(locator=self.A_PRODUCT_ADDED).text == self.product_name
     #
 #
+
---
+++ tests/test_main_page.py
@@ -0,0 +1,41 @@
+from time import sleep
+
+import allure
+import pytest
+
+
+class TestMainPage:
+
+    @pytest.mark.parametrize(
+        argnames="currency",
+        argvalues=[("EUR", "€"), ("GBP", "£"), ("USD", "$")],
+        ids=["Set currency: €", "Set currency: £", "Set currency: $"]
+    )
+    @allure.title("Check new currency has been applied")
+    @allure.story("Main page")
+    def test_change_currency(self, main_page, currency):
+        with allure.step("Set new currency"):
+            main_page.change_currency(new_currency=currency[0])
+        sleep(3)
+        with allure.step("Check new currency has been applied"):
+            assert main_page.get_current_currency() == currency[1]
+    #
+
+    @allure.title("Check My Account links")
+    @allure.story("Main page")
+    def test_open_my_account_dropdown(self, main_page):
+        with allure.step("Open My Account dropdown list"):
+            main_page.open_my_account_dropdown()
+        with allure.step("Check Register and Login links"):
+            assert main_page.find_element(locator=main_page.A_REGISTER)
+            assert main_page.find_element(locator=main_page.A_LOGIN)
+    #
+
+    def test_product_added_to_cart(self, main_page):
+        with allure.step("Check cart is empty"):
+            assert main_page.is_cart_empty()
+        with allure.step("Click add to cart button"):
+            main_page.add_to_cart()
+        with allure.step("Check product added to cart"):
+            main_page.check_product_added_to_cart()
+#
