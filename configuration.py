e7379f0d04e07b9ed4aa3ef21a04f1a9e55e0b3f
Alexander Knyazev <26230870+a13xk@users.noreply.github.com>
2020-08-24 12:33:38 +0300

    Add configuration

---
+++ configuration.py
@@ -0,0 +1,5 @@
+""" Various constants, helper classes and enumerations used by OpenCart """
+
+PAGE_TIMEOUT = 10   # Number of second to search for an element on page before raising TimeoutException
+OPENCART_ADMIN_USERNAME = "user"
+OPENCART_ADMIN_PASSWORD = "bitnami1"
