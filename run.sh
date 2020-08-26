7e2ef12cc500f0fd33a312278451e40656cd0bd0
Alexander Knyazev <a.knyazev@omprussia.ru>
2020-08-26 18:33:32 +0300

    Fix run script

--- run.sh
+++ run.sh
@@ -2,10 +2,13 @@
 
 rm -rf allure-*
 
-sudo rm ~/.aerokube/selenoid/logs/*
-sudo rm ~/.aerokube/selenoid/video/*
+if [! -z "$(ls -A ~/.aerokube/selenoid/logs)"]; then
+    sudo rm ~/.aerokube/selenoid/logs/*
+fi
+if [! -z "$(ls -A ~/.aerokube/selenoid/video)"]; then
+    sudo rm ~/.aerokube/selenoid/video/*
+fi
 
 pytest -v --alluredir=allure-report
 
 allure serve allure-report
-
